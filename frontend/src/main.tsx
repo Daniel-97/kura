fc6556271249c04c33f7ebb0a460808e259940e1
DZ <zeolla.d@gmail.com>
2026-06-11 12:43:23 +0200

    feat: wrap app with next-themes ThemeProvider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { ThemeProvider } from 'next-themes'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { HashRouter } from 'react-router-dom'
 import './i18n/index'
@@ -14,10 +15,12 @@ const queryClient = new QueryClient({
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <HashRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
-        <App />
-      </HashRouter>
-    </QueryClientProvider>
+    <ThemeProvider attribute="class" defaultTheme="light" disableTransitionOnChange>
+      <QueryClientProvider client={queryClient}>
+        <HashRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
+          <App />
+        </HashRouter>
+      </QueryClientProvider>
+    </ThemeProvider>
   </React.StrictMode>,
 )
